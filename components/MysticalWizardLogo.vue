020411a2e74dab55f6b865e6d508b54551cb3843
MysticalWizard <mmaring05@naver.com>
2021-07-17 16:38:40 +0900

    Add MysticalWizard Logo

---
+++ components/MysticalWizardLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img
+    class="MysticalWizardLogo"
+    alt="MysticalWizard Logo"
+    src="/MysticalWizardLogo.jpg"
+  >
+</template>
+
+<style>
+.MysticalWizardLogo {
+  height: 180px;
+  width: 180px;
+}
+</style>
