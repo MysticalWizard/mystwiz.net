a41f6bea8bcca73ce0b07e598edeac03a7b980a5
MysticalWizard <mmaring05@naver.com>
2021-08-21 01:29:07 +0900

    Update Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,82 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <MysticalWizardLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          MysticalWizard
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <div class="middle">
+          <h1 class="title" style="font-size: 80px !important;">MysticalWizard</h1>
+          <p class="subtitle">Programmer & Game Developer</p>
+          <ul class="links" style="font-size: 25px;">
+              <li>
+                  <a href="https://osu.ppy.sh/users/19430051/osu/" target="_blank" rel="noopener noreferrer">osu!</a>
+              </li>
+              <li>
+                  <a href="https://steamcommunity.com/id/mysticalwiz/" target="_blank" rel="noopener noreferrer">Steam</a>
+              </li>
+              <li>
+                  <a href="https://www.youtube.com/channel/UCGeXuxX5zk3T9KAhdpqRUfA/" target="_blank" rel="noopener noreferrer">YouTube</a>
+              </li>
+              <li>
+                  <a href="https://www.instagram.com/maring_shin/" target="_blank" rel="noopener noreferrer">Instagram</a>
+              </li>
+              <li>
+                  <a href="https://github.com/mysticalwizard/" target="_blank" rel="noopener noreferrer">GitHub</a>
+              </li>
+          </ul>
+      </div>
     </v-col>
   </v-row>
 </template>
+
+<style>
+.middle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    flex-direction: column;
+    color: #fff;
+}
+.middle,
+.middle .links {
+    display: flex;
+    justify-content: center;
+}
+.middle .links {
+    padding: 0;
+    margin: 0px;
+    list-style: none;
+    flex-wrap: wrap;
+    font-size: 18px;
+    -webkit-user-select: none; /* Safari */        
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* IE10+/Edge */
+    user-select: none; /* Standard */
+}
+.middle .links li {
+    display: inline;
+    padding: 0 10px;
+    font-weight: 300;
+}
+.middle .links li a {
+    color: #bdbdbd;
+    text-decoration: none;
+    transition-duration: 0.15s;
+}
+.middle .links li a:hover {
+    color: #fff !important;
+}
+.middle .title {
+    margin: 0 0 42px;
+    color: #fff;
+}
+.middle .subtitle,
+.middle .title {
+    font-weight: 300;
+    text-align: center;
+}
+.middle .subtitle {
+    letter-spacing: 4px;
+    color: #e2e2e2;
+}
+</style>
